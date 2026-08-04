861f7106ce4019c055d404ebbf316dd3fa9e69f0
Saisaran_p146 <saisaran158@gmail.com>
2026-08-04 11:43:46 +0530

    Initial commit

---
+++ src/main/java/com/kce/project/DisasteriqApplication.java
@@ -0,0 +1,13 @@
+package com.kce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasteriqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasteriqApplication.class, args);
+	}
+
+}
