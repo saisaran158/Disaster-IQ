6ec3efd1604691764c09876337793b861798e818
pradeepkumar2408 <pradeepkamaraj7639@gmail.com>
2026-08-04 12:24:57 +0530

    T

--- src/main/java/com/kce/project/DisasteriqApplication.java
+++ src/main/java/com/kce/project/DisasteriqApplication.java
@@ -8,7 +8,6 @@ public class DisasteriqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DisasteriqApplication.class, args);
-		System.out.println("Hello");
 	}
 
 }
